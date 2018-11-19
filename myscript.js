f3f8b3681c8099900e9c401ed3e2d49b73f17ab1
Afpa <elio007@hotmail.fr>
2018-11-19 16:33:12 +0100

    ajout commentaire pour expliquer le role des variables tabordi et tabuti dans la fonction randow dans ./myscript.js

--- myscript.js
+++ myscript.js
@@ -5,9 +5,12 @@ var couleur =['blue','red','green','orange','yellow','purple','pink'];
 
 var niveau = 0;
 
+//stockage de la sequence proposé par le jeu	
 var tabordi = [];
 
+//stockage  de la seqence proposé par l'utlisateur	
 var tabuti = [];	
-	
+
+
 }
 
